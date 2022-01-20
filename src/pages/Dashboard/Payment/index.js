95f663e0777b9dc626e4780654bcb7b7aab3827a
Pedro Lucas <pedrolucaspina22@gmail.com>
2022-01-20 09:21:34 -0300

    feat: add PaymentApi

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import useApi from "../../../hooks/useApi";
+
 export default function Payment() {
+  const [presenceTypes, setPresenceTypes] = useState([]);
+  const [hotelPlans, setHotelPlans] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const api = useApi();
+
+  function getInfo() {
+    api.payment.getPlansInfo().then(response => {
+      setHotelPlans(response.data.hotelPlans);
+      setPresenceTypes(response.data.presenceTypes);
+    }).catch(error => {
+      if (error.response) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage("Não foi possível conectar ao servidor!");
+      }
+    });
+  }
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
   return "Pagamento: Em breve!";
 }
