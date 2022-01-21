f901c78689bf850e8d7e87a942634818cbe15d0a
Anderson Aquino <andersonmr.aquino@gmail.com>
2022-01-21 10:34:17 -0300

    feat: payment confirmed component

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,21 +1,30 @@
 import { useState } from "react";
 import PlanSelection from "./PlanSelection";
 import PaymentInfo from "./PaymentInfo";
+import PaymentConfirmed from "./PaymentConfirmed";
 
 export default function Payment() {
   const [ chosenPresence, setChosenPresence ] = useState({});
   const [ chosenHotelPlan, setChosenHotelPlan ] = useState({});
   const [isTicketSelected, setIsTicketSelected] = useState(false);
+
+  const ticket = {};
     
-  return (
-    isTicketSelected ?
-      <PaymentInfo ticket={{ presence: chosenPresence, hotel: chosenHotelPlan }} />
-      : <PlanSelection
-        chosenHotelPlan = {chosenHotelPlan}
-        chosenPresence = {chosenPresence}
-        setChosenHotelPlan = {setChosenHotelPlan}
-        setChosenPresence={setChosenPresence}
-        setIsTicketSelected={setIsTicketSelected}
-      />
+  return ticket ? (
+    <PaymentConfirmed
+      ticket={{ presence: chosenPresence, hotel: chosenHotelPlan }}
+    />
+  ) : isTicketSelected ? (
+    <PaymentInfo
+      ticket={{ presence: chosenPresence, hotel: chosenHotelPlan }}
+    />
+  ) : (
+    <PlanSelection
+      chosenHotelPlan={chosenHotelPlan}
+      chosenPresence={chosenPresence}
+      setChosenHotelPlan={setChosenHotelPlan}
+      setChosenPresence={setChosenPresence}
+      setIsTicketSelected={setIsTicketSelected}
+    />
   );
 };
