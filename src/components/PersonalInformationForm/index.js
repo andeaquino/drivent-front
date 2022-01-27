87f87d5df6dc7eaa98ac756e596b5eb4e1c9f255
Anderson Aquino <andersonmr.aquino@gmail.com>
2022-01-27 13:58:27 -0300

    feat: add cpf validation on subscription form

--- src/components/PersonalInformationForm/index.js
+++ src/components/PersonalInformationForm/index.js
@@ -38,6 +38,11 @@ export default function PersonalInformationForm() {
     validations: FormValidations,
 
     onSubmit: (data) => {
+      if (!isValidCPF(data.cpf)) {
+        toast("Insira um cpf válido");
+        return;
+      }
+
       const newData = {
         name: data.name,
         cpf: data.cpf,
@@ -107,6 +112,31 @@ export default function PersonalInformationForm() {
       });
     });
   }, []);
+
+  function isValidCPF(cpf) {
+    let sum = 0;
+    let rest;
+    const cpfNum = cpf.replaceAll(".", "").replace("-", "");
+    if (cpfNum.match(/(\d)\1{10}/)) return false;
+
+    for (let i = 1; i <= 9; i++) {
+      sum = sum + parseInt(cpfNum.substring(i - 1, i)) * (11 - i);
+    }
+    rest = (sum * 10) % 11;
+      
+    if (rest === 10 || rest === 11) rest = 0;
+    if (rest !== parseInt(cpfNum.substring(9, 10))) return false;
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum = sum + parseInt(cpfNum.substring(i - 1, i)) * (12 - i);
+    } 
+    rest = (sum * 10) % 11;
+
+    if (rest === 10 || rest === 11) rest = 0;
+    if (rest !== parseInt(cpfNum.substring(10, 11))) return false;
+    return true;
+  }
 
   function isValidCep(cep) {
     return cep.length === 8;
