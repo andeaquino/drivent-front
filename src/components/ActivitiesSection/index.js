c98460c0d36584e7f1ff1ac19a21216cefe167ea
Anderson Aquino <andersonmr.aquino@gmail.com>
2022-01-31 10:45:15 -0300

    feat: add loading component on api requisitions

--- src/components/ActivitiesSection/index.js
+++ src/components/ActivitiesSection/index.js
@@ -2,24 +2,31 @@ import { useEffect, useState } from "react";
 import useApi from "../../hooks/useApi";
 import Days from "./Days";
 import ErrorContainer from "../ErrorContainer";
+import Loading from "../Dashboard/Loading";
 
 export default function Activities() {
   const [errorCode, setErrorCode] = useState(null);
   const [activitiesInfo, setActivitiesInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { activities } = useApi();
+
   useEffect(() => {
+    setLoading(true);
     activities
       .getActivities()
       .then((res) => {
         setActivitiesInfo(res.data);
+        setLoading(false);
       })
       .catch((e) => {
         setErrorCode(e.response.data.message);
+        setLoading(false);
       });
   }, []);
+
   return (
     <>
-      {!errorCode ? (
+      {loading ? <Loading /> : !errorCode ? (
         <Days list={activitiesInfo} />
       ) : (
         <ErrorContainer pageTitle="Escolha de atividades" errorMessage={errorCode} />
--- src/components/HotelInfo/index.js
+++ src/components/HotelInfo/index.js
@@ -4,26 +4,35 @@ import HotelList from "./HotelList";
 import ErrorContainer from "../ErrorContainer";
 import styled from "styled-components";
 import BookingInfos from "./BookingInfos";
+import Loading from "../Dashboard/Loading";
 
 export default function HotelInfo() {
   const [errorCode, setErrorCode] = useState(null);
   const [hotelInfo, setHotelInfo] = useState(null);
   const [isChangingRoom, setIsChangingRoom] = useState(false);
   const [isRoomChanged, setIsRoomChanged] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { booking } = useApi();
 
   useEffect(() => {
+    setLoading(true);
     booking
       .getBooking()
       .then((res) => {
         setHotelInfo(res.data);
+        setLoading(false);
       })
       .catch((e) => {
         setErrorCode(e.response.data);
+        setLoading(false);
       });
     setIsRoomChanged(false);
     setIsChangingRoom(false);
   }, [isRoomChanged]);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   if (errorCode) {
     return (
